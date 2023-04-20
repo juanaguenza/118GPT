19536203e35d3070fd574256077fbc836d61fa10
Juan Aguenza <juan@eduroam-169-233-228-148.ucsc.edu>
2023-04-20 12:42:47 -0700

    Fixed the infinite loop in baseline tests

--- Code/baseline_tests.py
+++ Code/baseline_tests.py
@@ -108,62 +108,57 @@ def create_result():
         return "error getting an answer"
 
 
-while True:
-    path_to_folders = '/Users/juan/Documents/CMPM118/MATH/test/' # replace with your own filepath
-    # folder = input("What folder would you like to run? (algebra) (counting_and_probability) (geometry) (intermediate_algebra) (number_theory) (prealgebra) or (precalculus)\n")
+path_to_folders = '/Users/juan/Documents/CMPM118/MATH/test/' # replace with your own filepath
+# folder = input("What folder would you like to run? (algebra) (counting_and_probability) (geometry) (intermediate_algebra) (number_theory) (prealgebra) or (precalculus)\n")
 
+list_of_folders = ["algebra", "counting_and_probability", "geometry", "intermediate_algebra", "number_theory", "prealgebra", "precalculus"]
 
-    list_of_folders = ["algebra", "counting_and_probability", "geometry", "intermediate_algebra", "number_theory", "prealgebra", "precalculus"]
+for folder in list_of_folders:
+    counter = 0
+    path_to_json = path_to_folders + folder + '/*.json'
 
-    for folder in list_of_folders:
-        counter = 0
-        path_to_json = path_to_folders + folder + '/*.json'
+    # make answers dir if it's not already there
+    try:
+        os.mkdir(path_to_json.replace("*.json", "") + "answers")
+    except OSError as error:
+        print(error)
+        pass
 
-        # make answers dir if it's not already there
-        try:
-            os.mkdir(path_to_json.replace("*.json", "") + "answers")
-        except OSError as error:
-            print(error)
-            pass
+    for f_name in sorted(glob(path_to_json)):
+        f = open(f_name)
 
-        for f_name in sorted(glob(path_to_json)):
-            f = open(f_name)
+        data = json.load(f)
 
-            data = json.load(f)
+        print("filepath we are working with is:")
+        print(f_name)
 
-            print("filepath we are working with is:")
-            print(f_name)
+        # get the name of the file here
+        file_num = f_name[len(path_to_json) - 6:]
+        file_num = file_num.strip(".json")
 
-            # get the name of the file here
-            file_num = f_name[len(path_to_json) - 6:]
-            file_num = file_num.strip(".json")
+        # get the problem from each file
+        message = {"role" : "system", "content" : data['problem']}
+        chat_messages.append(message)
 
-            # get the problem from each file
-            message = {"role" : "system", "content" : data['problem']}
-            chat_messages.append(message)
+        res = create_result()
 
-            res = create_result()
+        # delete the problem (don't keep track of old answers)
+        chat_messages.pop()
 
-            # delete the problem (don't keep track of old answers)
-            chat_messages.pop()
+        # now make it into the answers folder
+        answers_file = path_to_json.replace("*.json", "") + "answers/" + file_num + "_answer.json"
 
-            # now make it into the answers folder
-            answers_file = path_to_json.replace("*.json", "") + "answers/" + file_num + "_answer.json"
+        data_new = data
+        data_new['solution'] = res
 
-            data_new = data
-            data_new['solution'] = res
+        with open(answers_file, "w") as outfile:
+            outfile.write(json.dumps(data_new, indent = 4))
 
-            with open(answers_file, "w") as outfile:
-                outfile.write(json.dumps(data_new, indent = 4))
+        print("Completed: " + data_new['solution'])
+        counter += 1
+        print(str(counter) + " iterations")
 
-            print("Completed: " + data_new['solution'])
-            counter += 1
-            print(str(counter) + " iterations")
+        # trying to prevent reaching a RPM or TPM limit (might need to be longer, or might even be redundant and slowing down for no reason)
+        time.sleep(1)
 
-            # trying to prevent reaching a RPM or TPM limit (might need to be longer, or might even be redundant and slowing down for no reason)
-            time.sleep(1)
-
-
-    # try_again = input("Would you like to run this again? (Y/N)\n")
-    # if (try_again != 'Y'):
-    #     exit()
+print("We finished.")
